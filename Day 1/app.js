622c98001ad28d87b5b1f8c155024f677812fa20
Himanshu Bhatt <hbhatt034@gmail.com>
2024-01-05 16:44:50 +0530

    Added Css file and created nested elements using react

--- Day 1/app.js
+++ Day 1/app.js
@@ -1,7 +1,31 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!!"
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React!!"
+// );
+
+/**
+ * Creating a nested react element
+ *
+ * Something like:
+ *
+ * <div id = "parent">
+ *      <div id = "child">
+ *          <h1>This is a h1 tag</h1>
+ *      </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "This is a h1 tag")
+  )
 );
+
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("container"));
-root.render(heading);
+root.render(parent);
