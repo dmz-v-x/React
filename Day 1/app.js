8b7b7d35a27f0dc3d4898fbfd2559c9de04bf17c
Himanshu Bhatt <hbhatt034@gmail.com>
2024-01-05 16:50:01 +0530

    Nested element with same child

--- Day 1/app.js
+++ Day 1/app.js
@@ -16,14 +16,35 @@
  * </div>
  */
 
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "This is a h1 tag")
+//   )
+// );
+
+/**
+ * Creating nested elements with same child
+ * Something like:
+ *
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>This is a h1 tag</h1>
+ *          <h2>This is a h2 tag</h2>
+ *      </div>
+ * </div>
+ */
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "This is a h1 tag")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is a h1 tag"),
+    React.createElement("h2", {}, "This is a h2 tag"),
+  ])
 );
 
 console.log(parent);
