ac08d53e51a00c85fde26159073cffb1e3d9452d
Himanshu Bhatt <hbhatt034@gmail.com>
2024-01-08 12:27:08 +0530

    Creating React Element using JSX

--- Day 3/app.js
+++ Day 3/app.js
@@ -1,2 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Using React.createElement
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
+console.log(heading);
+
+// Using JSX
+
+const jsxHeading = <h1 id="heading">Hello World!</h1>;
+console.log(jsxHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
