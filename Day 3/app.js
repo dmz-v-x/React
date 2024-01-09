6c91dd2a632053d897753d2aab8e8c893332fa40
Himanshu Bhatt <hbhatt034@gmail.com>
2024-01-09 15:49:38 +0530

    Component Compositon

--- Day 3/app.js
+++ Day 3/app.js
@@ -1,23 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Using React.createElement
-
-const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
-console.log(heading);
-
-// Using JSX
-
-const jsxHeading = (
-  <h1 id="heading" tabIndex="1">
+const Title = () => (
+  <h1 className="head" tabIndex="1">
     Hello World!
   </h1>
 );
-console.log(jsxHeading);
 
-const HeadingComponent = () => {
-  return <h1 className="heading">This is a heading Component!</h1>;
-};
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">This is a heading Component!</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
