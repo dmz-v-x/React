e017120271e8e4661273ee7ed0be3d47743a0237
Himanshu Bhatt <hbhatt034@gmail.com>
2024-01-11 21:35:54 +0530

    Made some modification

--- FoodOrderingApp/app.js
+++ FoodOrderingApp/app.js
@@ -1284,24 +1284,9 @@ const Body = () => (
   <div className="body">
     <div className="search">Search</div>
     <div className="res-container">
-      <RestrauntCard resData={resList[0]} />
-      <RestrauntCard resData={resList[1]} />
-      <RestrauntCard resData={resList[2]} />
-      <RestrauntCard resData={resList[3]} />
-      <RestrauntCard resData={resList[4]} />
-      <RestrauntCard resData={resList[5]} />
-      <RestrauntCard resData={resList[6]} />
-      <RestrauntCard resData={resList[7]} />
-      <RestrauntCard resData={resList[8]} />
-      <RestrauntCard resData={resList[9]} />
-      <RestrauntCard resData={resList[10]} />
-      <RestrauntCard resData={resList[11]} />
-      <RestrauntCard resData={resList[12]} />
-      <RestrauntCard resData={resList[13]} />
-      <RestrauntCard resData={resList[14]} />
-      <RestrauntCard resData={resList[15]} />
-      <RestrauntCard resData={resList[16]} />
-      <RestrauntCard resData={resList[17]} />
+      {resList.map((restaurant) => (
+        <RestrauntCard key={restaurant.info.id} resData={restaurant} />
+      ))}
     </div>
   </div>
 );
