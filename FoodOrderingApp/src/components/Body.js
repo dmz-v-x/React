0529def019ca4f0da1a1b928a69a9f843e860aa6
Himanshu Bhatt <hbhatt034@gmail.com>
2024-01-13 20:49:04 +0530

    Added React Hook to our project

--- FoodOrderingApp/src/components/Body.js
+++ FoodOrderingApp/src/components/Body.js
@@ -1,15 +1,32 @@
 import resList from "../utils/mockData";
 import RestrauntCard from "./RestaurantCard";
+import { useState } from "react";
 
-const Body = () => (
-  <div className="body">
-    <div className="search">Search</div>
-    <div className="res-container">
-      {resList.map((restaurant) => (
-        <RestrauntCard key={restaurant.info.id} resData={restaurant} />
-      ))}
+const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter"
+          onClick={() => {
+            filteredList = listOfRestaurant.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurant(filteredList);
+          }}
+        >
+          Top Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurant.map((restaurant) => (
+          <RestrauntCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Body;
