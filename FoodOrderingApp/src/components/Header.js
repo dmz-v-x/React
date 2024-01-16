37dec6a9ff6483a11b1039da534cb816914d6c10
Himanshu Bhatt <hbhatt034@gmail.com>
2024-01-16 17:04:15 +0530

    Added error component and link component

--- FoodOrderingApp/src/components/Header.js
+++ FoodOrderingApp/src/components/Header.js
@@ -1,5 +1,6 @@
 import logo from "../../logo.jpg";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [btnNameReact, setbtnNameReact] = useState("Login");
@@ -11,9 +12,15 @@ const Header = () => {
 
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contacts</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
           <button
             className="Login"
